c82dd09b51c772516d1cb10d86b10a85f33e6233
Eduardo Hernández <eduardo@mymusclefactory.com>
2022-05-29 12:51:54 +0200

    fix - missing dependency

--- src/hooks/useCharacterEpisodes.tsx
+++ src/hooks/useCharacterEpisodes.tsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { Episode } from "../apis/rickAndMorty/episodes";
 import { fetchEpisodes } from "../application/rickAndMorty/rickAndMortySlice";
 import { AppDispatch, CustomState, RootState } from "../application/store";
+import { isNotRequested } from "../helpers/requestHelper";
 
 export const useCharacterEpisodes = (
   episodesIds: number[],
