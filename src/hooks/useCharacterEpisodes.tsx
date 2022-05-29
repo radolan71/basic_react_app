8bef52b90161e9f42a9769d9310508011cb809ac
Eduardo Hernández <eduardo@mymusclefactory.com>
2022-05-29 12:31:54 +0200

    clean up

--- src/hooks/useCharacterEpisodes.tsx
+++ src/hooks/useCharacterEpisodes.tsx
@@ -1,11 +1,8 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { RickAndMortyResponse } from "../apis/rickAndMorty/common";
 import { Episode } from "../apis/rickAndMorty/episodes";
 import { fetchEpisodes } from "../application/rickAndMorty/rickAndMortySlice";
 import { AppDispatch, CustomState, RootState } from "../application/store";
-import { Loader } from "../components/common/Loader/Loader";
-import { isLoading, isNotRequested } from "../helpers/requestHelper";
 
 export const useCharacterEpisodes = (
   episodesIds: number[],
